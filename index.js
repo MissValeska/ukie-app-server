e8d74fc96f8e40e0cfe93c825eb94bf7d383d7bd
Valeska <missvaleskavv@gmail.com>
2017-05-30 17:19:47 -0700

    fixed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express')
- 
 var app = express()
+
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/conj', function(req, res) {
   res.send("живеш")
@@ -18,4 +19,6 @@ app.get('/', function(req, res) {
   res.send("HAI")
 })
 
-app.listen(3000)
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
