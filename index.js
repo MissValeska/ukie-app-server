99a7fb250123b5a81e6e06a4acaae3c88fdfc6cf
Valeska <missvaleskavv@gmail.com>
2017-06-30 19:12:47 -0700

    .

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express')
+﻿var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000));
@@ -6,7 +6,8 @@ app.set('port', (process.env.PORT || 5000));
 app.set('json spaces', 0);
  
 app.get('/', function(req, res) {
-  res.json({conj:"живеш", conjtype:"ти", infin:"жити"});
+  res.sendfile('PrivacyPolicy.html', {root: __dirname })
+  #res.json({conj:"живеш", conjtype:"ти", infin:"жити"});
 })
 
 app.listen(app.get('port'), function() {
